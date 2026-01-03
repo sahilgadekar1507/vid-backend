c2519dc42eb08ae41711b914d311c140f770046e
sahilgadekar1507 <sahilgadekar15@gmail.com>
2026-01-03 19:18:00 +0530

    Utils - error handlers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import connectDB  from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`App is listening on the port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connecton failed !!!" , err);
+    
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// using promise
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+    
+}
+
+export {asyncHandler}
+
+// Using try-catch and async-await
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
